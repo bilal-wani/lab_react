ba5d81e479b53f5c74cc3d1f7d187ba9df862bc6
bilal <pro.bilal.wani@gmail.com>
2023-01-28 20:17:59 +0530

    changes in app.js

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <h2>Welcome to hell of learnig</h2>
+      <br/>
+      <p>
+        Enjoy the magic of Jenkins
+      </p>
     </div>
   );
 }
